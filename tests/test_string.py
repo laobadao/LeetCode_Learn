33dac353920da36a531143229d98fcec66af6507
junjunzhao <jjzhao@intenginetech.com>
2022-04-17 16:14:43 +0800

    update

--- tests/test_string.py
+++ tests/test_string.py
@@ -5,9 +5,9 @@ Authors
 """
 
 
-def test_():
+def test_replace_space():
     from string_lc.easy.replace_space import Solution
 
-    replaced_s = Solution().replaceSpace_v2("We are happy.")
+    replaced_s = Solution().replaceSpace_v3("We are happy.")
 
     assert replaced_s == r"We%20are%20happy."
